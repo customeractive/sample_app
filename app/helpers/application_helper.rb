310e85f6208c467422607fe206925c06e80ab6e0
Aaron Archambault <aaa@customeractive.com>
2011-04-19 14:05:33 -0400

    completed layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
+
+def logo
+logo = image_tag("logo.png", :alt => "Sample App", :class => "round") 
+
+end
 # return a title for each page.
+
+
+
 def title
 base_title = "Ruby on Rails Tutorial Sample App"
 if @title.nil?
