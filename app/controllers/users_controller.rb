1fc502e46322588bdbbd9251ccc8f7afeddc4ccf
Aaron Archambault <aaa@customeractive.com>
2011-05-04 01:28:23 -0400

    middel of signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
   def new
 @title = "Sign Up"
+ @user = User.new
   end
   def show
-@user = User.find (params[:id])
+@user = User.find(params[:id])
 @title = @user.name
 end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 SampleApp::Application.routes.draw do
+  get "sessions/new"
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destrory]
+
+
   
 root :to => "pages#home"
 
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
