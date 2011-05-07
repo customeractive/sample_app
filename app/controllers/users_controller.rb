699f71b73b93f392167090099c754740a9be0edd
Aaron Archambault <aaa@customeractive.com>
2011-05-07 12:15:38 -0400

    user index in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   def new
 @title = "Sign Up"
  @user = User.new
@@ -8,7 +10,7 @@ class UsersController < ApplicationController
 @title = @user.name
 end
   def create
-    @user = User.new(params[:user])
+   # @user = User.new(params[:user])
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -18,5 +20,34 @@ end
       render 'new'
     end
   end
+def edit
+ # @user = User.find(params[:id])
+  @title = "Edit user"
+end
+def update
+ @user = User.find(params[:id])
+ if @user.update_attributes(params[:user])
+   flash[:success] = "Profile updated."
+   redirect_to @user
+ else
+    @title = "Edit user"
+    render 'edit'
+ end
 
+end
+def index
+  @title = "All users"
+  @users = User.all
+end
+
+
+
+private
+def authenticate
+  deny_access unless signed_in?
+end
+def correct_user
+   @user = User.find(params[:id])
+ redirect_to(root_path) unless current_user?(@user)
+end
 end
